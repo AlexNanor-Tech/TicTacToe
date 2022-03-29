8e74379c7655249ee3cf5f18a68914e6a44e8566
alexnanor <73364939+AlexNanor-Tech@users.noreply.github.com>
2022-03-29 08:11:51 -0400

    Update TicTacToe.java
    Game with a couple bugs

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,15 @@
-import java.util.Scanner;
-import java.util.Random; 
+import java.util.*;
+
+// Bugs:
+//
+// Off by one error for user placing on grid
+// Must handle edge case where cpu overwrites player position
 
 public class TicTacToe {
+
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+
 
     public static void printGameBoard(char gameBoard[][]){
         for(char[] row: gameBoard){
@@ -15,17 +23,14 @@ public class TicTacToe {
 
     public static void placePiece(char gameBoard[][], int pos, String user) {
         char  symbol = 'n';
-        System.out.println(symbol);
+
         if(user.equals("player")){
             symbol = 'X';
+            playerPositions.add(pos);
         } else if(user.equals("cpu")){
             symbol = '0';
+            cpuPositions.add(pos);
         }
-        switch (pos) { // If pos, place on board.
-            case 1:
-                gameBoard[0][0] = symbol;
-                break;
-            case 2:
                 switch (pos) { // If pos, place on board.
                     case 1:
                         gameBoard[0][0] = symbol;
@@ -60,8 +65,41 @@ public class TicTacToe {
 
         }
 
+    public static String checkWinner(){
+        List topRow = Arrays.asList(1,2,3);
+        List midRow = Arrays.asList(4,5,6);
+        List botRow = Arrays.asList(7,8,9);
 
+        List leftCol = Arrays.asList(1,4,7);
+        List midCol = Arrays.asList(2,5,8);
+        List rightCol = Arrays.asList(3,6,9);
+
+        List diag = Arrays.asList(1,5,9);
+        List revDiag = Arrays.asList(7,5,3);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(rightCol);
+        winning.add(midCol);
+        winning.add(diag);
+        winning.add(revDiag);
+        for(List l: winning){
+            if(playerPositions.containsAll(l)){
+                return "Congratulations, you won!";
+            } else if(cpuPositions.containsAll(l)){
+                return "I'm sorry. you lost. CPU wins! :(";
+            } else if(playerPositions.size() + cpuPositions.size() == 9){
+                return "CAT!";
+            }
+        }
+        return "";
     }
+
+
+
 
     public static void main(String[] args){
         char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
@@ -74,24 +112,32 @@ public class TicTacToe {
 
         printGameBoard(gameBoard);
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your placement (1-9): ");
-        int  pos = scan.nextInt();
 
-        System.out.println(pos);
+        while(true){
+            System.out.println("Enter your placement (1-9): ");
+            int  pos = scan.nextInt();
 
-//        changing a place is simply changing a grid position to an x or an o:
-//        remember, in a 2D array we get rc (row, column)
-        placePiece(gameBoard, pos, "player");
-        Random rand = new Random();
-        placePiece(gameBoard, pos, "cpu");
-        placePiece(gameBoard, pos, "player");
-        placePiece(gameBoard, pos, "cpu");
-        placePiece(gameBoard, pos, "player");
-        printGameBoard(gameBoard);
+            System.out.println(pos);
+
+
+            placePiece(gameBoard, pos, "player");
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9) + 1;
+            placePiece(gameBoard, cpuPos, "cpu");
+            printGameBoard(gameBoard);
+
+            System.out.println(checkWinner());
+
+        }
+
+
+
+
 
 
 
     }
+
 
 
 };
