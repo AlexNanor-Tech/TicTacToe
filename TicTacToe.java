8ede486fd38672df4c1d354da5e48d4cf7a824a1
alexnanor <73364939+AlexNanor-Tech@users.noreply.github.com>
2022-03-31 16:03:59 -0400

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -37,25 +37,25 @@ public class TicTacToe {
                         gameBoard[0][2] = symbol;
                         break;
                     case 3:
-                        gameBoard[0][2] = symbol;
-                        break;
-                    case 4:
                         gameBoard[0][4] = symbol;
                         break;
-                    case 5:
+                    case 4:
                         gameBoard[2][0] = symbol;
                         break;
-                    case 6:
+                    case 5:
                         gameBoard[2][2] = symbol;
                         break;
-                    case 7:
+                    case 6:
                         gameBoard[2][4] = symbol;
                         break;
-                    case 8:
+                    case 7:
                         gameBoard[4][0] = symbol;
                         break;
-                    case 9:
+                    case 8:
                         gameBoard[4][2] = symbol;
+                        break;
+                    case 9:
+                        gameBoard[4][4] = symbol;
                         break;
                     default:
                         break;
